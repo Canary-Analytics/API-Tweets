968aa5934425ea2e81a75048bf420a4f5209dc8c
Samuel Ramos <alu0100815146@ull.edu.es>
2017-02-11 21:40:40 +0000

    Añadiendo plantilla y mejorando Front-End

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-var express = require('express');
-var passport = require('passport');
-var Strategy = require('passport-twitter').Strategy;
+const express = require('express');
+const passport = require('passport');
+const Strategy = require('passport-twitter').Strategy;
 const Twitter = require('twitter');
-var client;
+const app = express();
+let client;
 
 passport.use(new Strategy({
         consumerKey: 'aDay6jhWc9b3y3Z31BRx6uXVc',
         consumerSecret: '6vbURPCBJSSQakw2t6eHvAY01HBong3rjnQbtGcr34ZNateN0s',
         callbackURL: 'http://localhost:8080/login/twitter/return'
     },
-    function(token, tokenSecret, profile, cb) {
+    (token, tokenSecret, profile, cb) => {
         client = new Twitter({
             consumer_key: 'aDay6jhWc9b3y3Z31BRx6uXVc',
             consumer_secret: '6vbURPCBJSSQakw2t6eHvAY01HBong3rjnQbtGcr34ZNateN0s',
@@ -19,18 +20,17 @@ passport.use(new Strategy({
 
         return cb(null, profile);
     }));
-passport.serializeUser(function(user, cb) {
+
+passport.serializeUser((user, cb) => {
     cb(null, user);
 });
 
-passport.deserializeUser(function(obj, cb) {
+passport.deserializeUser((obj, cb) => {
     cb(null, obj);
 });
 
-
-var app = express();
 app.use(express.static(__dirname + '/public'));
-
+app.set('port', (process.env.PORT || 8080));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(require('morgan')('combined'));
@@ -47,50 +47,39 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-app.get('/',
-    function(req, res) {
-        res.render('home', {
-            user: req.user
-        });
-    });
+app.get('/', (req, res) => {
+    res.render('home', { user: req.user });
+});
 
-app.get('/login',
-    function(req, res) {
-        res.render('login');
-    });
+app.get('/login', (req, res) => {
+  res.render('login');
+});
 
-app.get('/login/twitter',
-    passport.authenticate('twitter'));
+app.get('/login/twitter', passport.authenticate('twitter'));
 
-app.get('/login/twitter/return',
-    passport.authenticate('twitter', {
-        failureRedirect: '/login'
-    }),
-    function(req, res) {
+app.get('/login/twitter/return', passport.authenticate('twitter', { failureRedirect: '/login' }), (req, res) => {
         res.redirect('/');
-    });
+});
 
-app.get('/profile',
-    require('connect-ensure-login').ensureLoggedIn(),
-    function(req, res) {
+app.get('/profile', require('connect-ensure-login').ensureLoggedIn(), (req, res) => {
+  res.render('profile', { user: req.user });
+});
 
-    });
-
-app.get('/datos',
-    require('connect-ensure-login').ensureLoggedIn(),
-    function(req, res) {
-        res.render('datos');
-    });
+app.get('/busqueda', require('connect-ensure-login').ensureLoggedIn(), (req, res) => {
+  res.render('busqueda');
+});
 
 app.post('/search', require('connect-ensure-login').ensureLoggedIn(), (req, res) => {
     console.log("Valor 1: " + req.body.valor.v1);
     client.get('search/tweets', {
         q: req.body.valor.v1
-    }, function(error, tweets, response) {
+    }, (error, tweets, response) => {
         for (var i = 0; i < tweets.statuses.length; i++) {
             console.log(tweets.statuses[i].user.screen_name);
         }
     });
 })
 
-app.listen(8080);
+app.listen(app.get('port'), () => {
+    console.log(`Node app is running at localhost: ${app.get('port')}` );
+});
