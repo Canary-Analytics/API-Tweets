1e11e7b38de380d5b5defb74252c160f462661a0
Samuel Ramos <alu0100815146@ull.edu.es>
2017-02-12 18:42:25 +0000

    Updates

--- index.js
+++ index.js
@@ -4,27 +4,27 @@ const Strategy = require('passport-twitter').Strategy;
 const Twitter = require('twitter');
 const app = express();
 const client = new Twitter({
-    consumer_key: 'aDay6jhWc9b3y3Z31BRx6uXVc',
-    consumer_secret: '6vbURPCBJSSQakw2t6eHvAY01HBong3rjnQbtGcr34ZNateN0s',
-    access_token_key: '827584840677617664-GCqacrqLnyTUcYUTkHzP4tpwYb7wW5y',
-    access_token_secret: '9rPagouJR4nUgbvrgmUQH8EISDEknl0s7ipeBPwuZB8NC'
+  consumer_key: 'aDay6jhWc9b3y3Z31BRx6uXVc',
+  consumer_secret: '6vbURPCBJSSQakw2t6eHvAY01HBong3rjnQbtGcr34ZNateN0s',
+  access_token_key: '827584840677617664-GCqacrqLnyTUcYUTkHzP4tpwYb7wW5y',
+  access_token_secret: '9rPagouJR4nUgbvrgmUQH8EISDEknl0s7ipeBPwuZB8NC'
 });
 
 passport.use(new Strategy({
-        consumerKey: 'aDay6jhWc9b3y3Z31BRx6uXVc',
-        consumerSecret: '6vbURPCBJSSQakw2t6eHvAY01HBong3rjnQbtGcr34ZNateN0s',
-        callbackURL: 'http://localhost:8080/login/twitter/return'
-    },
-    (token, tokenSecret, profile, cb) => {
-        return cb(null, profile);
-    }));
+    consumerKey: 'aDay6jhWc9b3y3Z31BRx6uXVc',
+    consumerSecret: '6vbURPCBJSSQakw2t6eHvAY01HBong3rjnQbtGcr34ZNateN0s',
+    callbackURL: 'http://localhost:8080/login/twitter/return'
+  },
+  (token, tokenSecret, profile, cb) => {
+    return cb(null, profile);
+}));
 
 passport.serializeUser((user, cb) => {
-    cb(null, user);
+  cb(null, user);
 });
 
 passport.deserializeUser((obj, cb) => {
-    cb(null, obj);
+  cb(null, obj);
 });
 
 app.use(express.static(__dirname + '/public'));
@@ -34,19 +34,19 @@ app.set('view engine', 'ejs');
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({
-    extended: true
+  extended: true
 }));
 app.use(require('express-session')({
-    secret: 'keyboard cat',
-    resave: true,
-    saveUninitialized: true
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
 }));
 app.use(passport.initialize());
 app.use(passport.session());
 
 
 app.get('/', (req, res) => {
-    res.render('home');
+  res.render('home');
 });
 
 app.get('/login', (req, res) => {
@@ -56,11 +56,10 @@ app.get('/login', (req, res) => {
 app.get('/login/twitter', passport.authenticate('twitter'));
 
 app.get('/login/twitter/return', passport.authenticate('twitter', { failureRedirect: '/login' }), (req, res) => {
-        res.redirect('/profile');
+  res.redirect('/profile');
 });
 
 app.get('/profile', require('connect-ensure-login').ensureLoggedIn(), (req, res) => {
-  console.log(req.user._json);
   res.render('profile', { user: req.user._json });
 });
 
@@ -71,7 +70,6 @@ app.get('/busqueda', require('connect-ensure-login').ensureLoggedIn(), (req, res
 app.post('/search', require('connect-ensure-login').ensureLoggedIn(), (req, res) => {
     client.get('search/tweets', { q: req.body.valor.v1 }, (error, tweets, response) => {
         res.render('resultados', { resultado: tweets.statuses });
-
     });
 });
 
